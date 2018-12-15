b177877e72e27b778214cd19e10ebbaba5698717
Alex Houser <alexanderhouser@gmail.com>
2018-12-15 14:02:27 -0600

    commit1

--- Idea-Storage/Controllers/HomeController.cs
+++ Idea-Storage/Controllers/HomeController.cs
@@ -13,16 +13,9 @@ namespace Idea_Storage.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult NewIdea()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
